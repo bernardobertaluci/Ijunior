46eb85492ef50b8d263b77a62fbdf1de2f2ca6f8
Danil Alexeev <bernardobertalucii@gmail.com>
2022-03-25 13:21:29 +0700

    Task07

---
+++ Task07/Task07/Task07/Program.cs
@@ -0,0 +1,20 @@
+﻿class Weapon
+{
+    private readonly int _bulletsPerShot;
+    private int _bullets;
+    private int _bulletAfterShot => _bullets - _bulletsPerShot;
+
+    public Weapon(int bulletsPerShot, int bullets)
+    {
+        _bulletsPerShot = bulletsPerShot;
+        _bullets = bullets;
+    }
+
+    public bool CanShoot => _bulletAfterShot > 0;
+
+    public void Shoot()
+    {
+        if(CanShoot)
+            _bullets = _bulletAfterShot;
+    }  
+}
