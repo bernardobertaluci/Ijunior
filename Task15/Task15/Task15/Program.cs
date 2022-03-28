1da6384f4f352c0fef927442716115b03295e5b5
Danil Alexeev <bernardobertalucii@gmail.com>
2022-03-28 15:36:44 +0700

    Task15

---
+++ Task15/Task15/Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task15
+{
+    internal class Program
+    {
+        public void Shoot(Player player)
+        {
+            if (player == null)
+                new ArgumentNullException(nameof(player));
+        }
+        public string Find(int number) 
+        {
+            if(number < 0)
+                throw new ArgumentOutOfRangeException(nameof(number));
+
+            return string;
+        }
+    }
+}
